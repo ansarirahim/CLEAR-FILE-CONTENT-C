98f4f4b6bc15c5114166a02159d5d665125e18b3
AbdulRaheem <ansarirahim1@gmail.com>
2021-02-13 22:58:22 +0530

    Update clearFileContent.c

--- clearFileContent.c
+++ clearFileContent.c
@@ -1,11 +1,21 @@
+char debugFilePath[100];
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #define __DEBUG_FILE_PATH_INDEX__ 1
 int main(int argc, char **argv)
 {
-int ch=0;
-//char str[100];
+    int ch = 0;
+    //char str[100];
 
-
-for (ch = 0; ch < 100; ch++)
+    for (ch = 0; ch < 100; ch++)
     {
         if (argv[__DEBUG_FILE_PATH_INDEX__][ch] == '\0')
             break;
@@ -13,4 +23,4 @@ for (ch = 0; ch < 100; ch++)
     }
     fclose(fopen(debugFilePath, "w"));
     return 0;
-    }
+}
