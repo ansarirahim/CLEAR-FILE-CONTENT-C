9bf634ddc0619dad009b348b8044ad8dc1745306
AbdulRaheem <ansarirahim1@gmail.com>
2021-02-13 22:47:49 +0530

    Create clearFileContent.c

---
+++ clearFileContent.c
@@ -0,0 +1,16 @@
+#define __DEBUG_FILE_PATH_INDEX__ 1
+int main(int argc, char **argv)
+{
+int ch=0;
+//char str[100];
+
+
+for (ch = 0; ch < 100; ch++)
+    {
+        if (argv[__DEBUG_FILE_PATH_INDEX__][ch] == '\0')
+            break;
+        debugFilePath[ch] = argv[__DEBUG_FILE_PATH_INDEX__][ch]; //
+    }
+    fclose(fopen(debugFilePath, "w"));
+    return 0;
+    }
